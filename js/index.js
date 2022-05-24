6b927be0d39b1e4eae2e820abfcea367082f1796
CodeMagnet69 <leandergjerstad@gmail.com>
2022-05-24 19:52:22 +0200

    Added keydown function, you can now continue calculating after prev calcuation

--- js/index.js
+++ js/index.js
@@ -1,6 +1,12 @@
 let userInput = [];
 
+let keyMap = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ".", "+", "-", "*", "/", "Backspace", "="]
+
 buttonArray = document.querySelectorAll("button")
+
+//Denne koden er et dårlig eksempel på Do not repeat yourself,
+//men jeg prøvde bare å gjøre oppgaven så raskt som mulig, derfor er ikke koden optimal
+//og dårlig planlagt.
 
 for (const element of buttonArray) {
     element.addEventListener("click", function calculator(event) {
@@ -8,25 +14,56 @@ for (const element of buttonArray) {
         let resultField = document.getElementById("numOutput");
         let eventID = event.target.id;
 
-        if(eventID === "="){
+        if (eventID === "=") {
             let calculated = eval(userInput.join(""));
-            resultField.innerHTML= userInput.join("") + " = " + calculated;
-            
-        }else{
-            if(eventID === "backSpace"){
+            resultField.innerHTML = calculated;
+
+            userInput = [];
+            userInput.push(calculated);
+
+        } else {
+            if (eventID === "backSpace") {
                 userInput.pop();
-                resultField.innerHTML= userInput.join("");
-            }else{
-                if(eventID === "reset"){
+                resultField.innerHTML = userInput.join("");
+            } else {
+                if (eventID === "reset") {
                     userInput = [];
-                    resultField.innerHTML= userInput.join("");
-                }else{
+                    resultField.innerHTML = userInput.join("");
+                } else {
                     userInput.push(eventID);
-                    resultField.innerHTML= userInput.join("");
+                    resultField.innerHTML = userInput.join("");
                 }
+            }
+        }
+    });
+}
+
+document.addEventListener("keydown", function (event) {
+    let keyInput = event.key;
+    let resultField = document.getElementById("numOutput");
+    console.log(keyInput);
+
+    if (keyInput === "Backspace") {
+        userInput.pop();
+        resultField.innerHTML = userInput.join("");
+
+    } else {
+
+        if (keyInput === "=") {
+            let calculated = eval(userInput.join(""));
+            resultField.innerHTML = calculated;
+
+            userInput = [];
+            userInput.push(calculated);
+
+        } else {
+            if (keyMap.includes(keyInput)) {
+                userInput.push(keyInput);
+                document.getElementById("numOutput").innerHTML = userInput.join("");
             }
         }
 
 
-    });
-}
+    }
+
+})
