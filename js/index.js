04e0281283e15ed58ada0586a96051db71c6fa3b
CodeMagnet69 <leandergjerstad@gmail.com>
2022-05-24 13:02:09 +0200

    Finished +,-,/,*, reset and backspace functions, Project essentially finished

--- js/index.js
+++ js/index.js
@@ -1,9 +1,32 @@
-numArray = []
+let userInput = [];
 
 buttonArray = document.querySelectorAll("button")
 
-for(element in buttonArray){
-    addEventListener("click", function calculator(e){
-        console.log("Clicked")
+for (const element of buttonArray) {
+    element.addEventListener("click", function calculator(event) {
+
+        let resultField = document.getElementById("numOutput");
+        let eventID = event.target.id;
+
+        if(eventID === "="){
+            let calculated = eval(userInput.join(""));
+            resultField.innerHTML= userInput.join("") + " = " + calculated;
+            
+        }else{
+            if(eventID === "backSpace"){
+                userInput.pop();
+                resultField.innerHTML= userInput.join("");
+            }else{
+                if(eventID === "reset"){
+                    userInput = [];
+                    resultField.innerHTML= userInput.join("");
+                }else{
+                    userInput.push(eventID);
+                    resultField.innerHTML= userInput.join("");
+                }
+            }
+        }
+
+
     });
 }
